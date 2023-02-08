2823ac2e932f7451e176115c4f961a3707d2508e
zionexeonch <zionexeon@gmail.com>
2023-02-08 11:38:23 +0700

    laporan

--- resources/js/Pages/LaporanSuperAdmin/RincianLaporan.js
+++ resources/js/Pages/LaporanSuperAdmin/RincianLaporan.js
@@ -72,7 +72,7 @@ const RincianLaporan = (props) => {
           </tbody>
         </table>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-4 mb-4">
         <div className="space-y-6">
           <div>
             <label
@@ -168,6 +168,49 @@ const RincianLaporan = (props) => {
             </button>
           </div>
         </div>
+      </div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg -scroll-mt-3">
+        <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                #
+              </th>
+              <th scope="col" className="px-6 py-3">
+                No Faktur
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nama Customer
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Alamat Pengiriman
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Total Harga
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Total Item
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {props.orders.idToko}
+              </th>
+              <td className="px-6 py-4">{props.orders.noFaktur}</td>
+              <td className="px-6 py-4">{props.orders.namaCostumer}</td>
+              <td className="px-6 py-4">{props.orders.alamatPengiriman}</td>
+              <td className="px-6 py-4">
+                {formatter.format(props.orders.total)}
+              </td>
+              <td className="px-6 py-4">{props.orders.totalItem}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
